48b71899fdb89730be92cf2701dda85e4fb2a4ff
muhammadusman349 <usmankhadim.uk.uk@gmail.com>
2024-07-11 10:33:34 +0500

    added first

---
+++ src/conf/celery.py
@@ -0,0 +1,17 @@
+import os
+
+from celery import Celery
+# from celery.schedules import crontab
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "conf.settings")
+
+app = Celery("conf")
+
+# Using a string here means the worker don't have to serialize
+# the configuration object to child processes.
+# - namespace='CELERY' means all celery-related configuration keys
+#   should have a `CELERY_` prefix.
+app.config_from_object("django.conf:settings", namespace="CELERY")
+
+# Load task modules from all registered Django app configs.
+app.autodiscover_tasks()
