2645bc7c74c3f58020a546cbbbe69391f7835f4b
muhammadusman349 <usmankhadim.uk.uk@gmail.com>
2024-07-27 23:25:43 +0500

    post crud added in

--- src/post/admin.py
+++ src/post/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
-from .models import *
+from .models import Post, Customer
 
 # Register your models here.
 
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'created_at', 'updated_at')
+    search_fields = ('title', 'content')
+    prepopulated_fields = {'slug': ('title',)}
+
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(Customer)
