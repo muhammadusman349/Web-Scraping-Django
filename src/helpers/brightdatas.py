fb8b1638aa83fa8eece4dc9ddf94e9352c1b6747
muhammad usman <usmankhadim.uk.uk@gmail.com>
2024-11-07 19:56:14 +0500

    Captcha solve logic and extract amazon product data are done

--- src/helpers/brightdatas.py
+++ src/helpers/brightdatas.py
@@ -1,24 +1,31 @@
 from selenium.webdriver import Remote, ChromeOptions
 from selenium.webdriver.chromium.remote_connection import ChromiumRemoteConnection
+from selenium.webdriver.common.by import By
+from urllib.parse import urljoin, urlparse
 from decouple import config
 
 SBR_WEBDRIVER = config('SBR_WEBDRIVER', default=None)
 
 
-def scrape(url=None):
+def scrape(url=None, body_only=True, solve_captcha=False, wait_seconds=0):
     print('Connecting to Scraping Browser...')
     sbr_connection = ChromiumRemoteConnection(SBR_WEBDRIVER, 'goog', 'chrome')
     html = ""
+    url = urljoin(url, urlparse(url).path)
     with Remote(sbr_connection, options=ChromeOptions()) as driver:
         print(f'Connected! Navigating to {url}')
         driver.get(url)
-        # CAPTCHA handling: If you're expecting a CAPTCHA on the target page, use the following code snippet to check the status of Scraping Browser's automatic CAPTCHA solver
-        # print('Waiting captcha to solve...')
-        # solve_res = driver.execute('executeCdpCommand', {
-        #     'cmd': 'Captcha.waitForSolve',
-        #     'params': {'detectTimeout': 10000},
-        # })
-        # print('Captcha solve status:', solve_res['value']['status'])
+        if wait_seconds > 0:
+            driver.implicitly_wait(wait_seconds)
+        if solve_captcha:
+            solve_res = driver.execute('executeCdpCommand', {
+                'cmd': 'Captcha.waitForSolve',
+                'params': {'detectTimeout': 10000},
+            })
+            print('Captcha solve status:', solve_res['value']['status'])
         print('Navigated! Scraping page content...')
         html = driver.page_source
+        if body_only:
+            body = driver.find_element(By.TAG_NAME, "body")
+            html = body.get_attribute('innerHTML')
     return html
